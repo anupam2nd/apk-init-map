85894eaabae917fc511e566d4e1fd10e201a5494
SAMPURNA-DEVELOPER <das.anupam70@gmail.com>
2025-08-26 18:44:44 +0530

    integrated google map

--- App.jsx
+++ App.jsx
@@ -17,8 +17,10 @@ import {
   SafeAreaView,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { PermissionsAndroid, Platform, BackHandler } from 'react-native';
 
 import AppNavigator from './src/navigation/AppNavigator';
+import { useEffect } from 'react';
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
@@ -34,6 +36,39 @@ function AppContent() {
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Location Permission',
+          message: 'This app needs access to your location',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('Location permission granted');
+        return true;
+      } else {
+        console.log('Location permission denied');
+        // BackHandler.exitApp();
+        return false;
+      }
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    requestLocationPermission().then(res => {
+      // if(!res){
+      //   BackHandler.exitApp()
+      // }
+    });
+  }, []);
 
   return (
     <SafeAreaProvider>
--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -1,17 +1,58 @@
-import { Text, Button, View } from 'react-native';
+import { use, useState } from 'react';
+import { Text, Button, View, StyleSheet } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 function MapScreen() {
+  const [location, setLocation] = useState(null);
 
-const fetchLocation = () => {
-    console.log('location')
-}
+  const fetchLocation = () => {
+    Geolocation.getCurrentPosition(postion => {
+      console.log(postion.coords);
+    });
+    console.log('location');
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Map Screen</Text>
-      <Button title='Fetch Location' onPress={fetchLocation} />
+      <Button title="Fetch Location" onPress={fetchLocation} />
+      <View
+        style={{
+          borderColor: 'black',
+          borderWidth: 1,
+          width: '100%',
+          height: 700,
+        }}
+      >
+        <MapView
+          style={[styles.map, StyleSheet.absoluteFill]}
+          initialRegion={{
+            latitude: 22.5726,
+            longitude: 88.3639,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          provider={PROVIDER_GOOGLE}
+          showsMyLocationButton={true}
+          toolbarEnabled={true}
+          zoomControlEnabled={Platform.OS === 'android'}
+        >
+          <Marker coordinate={{ latitude: 22.5726, longitude: 88.3639 }} />
+        </MapView>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
